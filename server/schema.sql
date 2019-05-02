69eb769ec2435f8e1b1424f0229fdcfb7320533c
weitsesun <wtse.sun@gmail.com>
2019-05-01 21:01:05 -0700

    Implement users, messages, rooms table

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,39 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  user_id int not null auto_increment primary key
 );
+
+CREATE TABLE rooms (
+  room_id int not null auto_increment primary key,
+  name VARCHAR(15)
+);
+
+CREATE TABLE messages (
+  msgid int not null auto_increment primary key,
+  name VARCHAR(20),
+  date date,
+  text text,
+  user_id int not null,
+  room_id int not null,
+  FOREIGN KEY (room_id) REFERENCES rooms(room_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+/* -- 
+  -- date date,
+  -- text text,
+  -- ;
+  --  */
+  /* Describe your table here.*/
 
 /* Create other tables and define schemas for them here! */
 
+/* status == away/active */
+/*line status message */
 
 
 
